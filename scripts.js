2a38f2656749639110806e4f8466da3d8a2507a3
sx2gw5t77x <18550862-sx2gw5t77x@users.noreply.replit.com>
2023-05-03 01:47:57 +0000

    added authentication and outage entry.

--- scripts.js
+++ scripts.js
@@ -1,121 +1,184 @@
-const calendar = document.getElementById('calendar');
-const operationalDays = document.getElementById('operationalDays');
-const outageDays = document.getElementById('outageDays');
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-app.js";
+import { getFirestore, collection, getDocs, addDoc, updateDoc, doc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-firestore.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-auth.js';
 
-const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const firebaseConfig = {
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id"
+};
 
-let lastSelectedDay = null;
-let shiftKeyPressed = false;
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth();
 
-document.addEventListener('keydown', (event) => {
-  if (event.key === 'Shift') {
-    shiftKeyPressed = true;
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const loginBtn = document.getElementById('login');
+const signupBtn = document.getElementById('signup');
+const logoutBtn = document.getElementById('logout');
+const loginForm = document.getElementById('login-form');
+
+// Sign up
+signupBtn.addEventListener('click', async () => {
+  try {
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    await createUserWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    alert(error.message);
   }
 });
 
-document.addEventListener('keyup', (event) => {
-  if (event.key === 'Shift') {
-    shiftKeyPressed = false;
+// Login
+loginBtn.addEventListener('click', async () => {
+  try {
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    alert(error.message);
   }
 });
 
-function generateCalendar() {
-  for (let month = 0; month < 12; month++) {
-    const monthElement = document.createElement('div');
-    monthElement.className = 'month';
-
-    const monthName = document.createElement('div');
-    monthName.className = 'month-name';
-    monthName.textContent = monthNames[month];
-    monthElement.appendChild(monthName);
-
-    const daysElement = document.createElement('div');
-    daysElement.className = 'days';
-
-    const firstDay = new Date(2023, month, 1).getDay();
-    const lastDate = new Date(2023, month + 1, 0).getDate();
-
-    for (let i = 1; i <= 42; i++) {
-      const dayElement = document.createElement('div');
-      dayElement.className = 'day';
-      
-      if (i > firstDay && i <= lastDate + firstDay) {
-        const dayNumber = i - firstDay;
-        dayElement.textContent = dayNumber;
-        dayElement.dataset.date = `2023-${month + 1}-${dayNumber}`;
-        
-        dayElement.addEventListener('click', () => {
-          if (shiftKeyPressed && lastSelectedDay) {
-            selectRange(lastSelectedDay, dayElement);
-          } else {
-            dayElement.classList.toggle('outage');
-          }
-          if (!shiftKeyPressed) {
-            lastSelectedDay = dayElement;
-          }
-          updateDaysCount();
-          saveState();
-        });
-      } else {
-        dayElement.style.visibility = 'hidden';
-      }
-
-      daysElement.appendChild(dayElement);
-    }
-
-    monthElement.appendChild(daysElement);
-    calendar.appendChild(monthElement);
+// Logout
+logoutBtn.addEventListener('click', async () => {
+  try {
+    await signOut(auth);
+  } catch (error) {
+    alert(error.message);
   }
-}
+});
 
-function updateDaysCount() {
-  const totalDays = document.querySelectorAll('.day[data-date]').length;
-  const outageDaysCount = document.querySelectorAll('.day.outage').length;
-  const operationalDaysCount = totalDays - outageDaysCount;
+// Auth state listener
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in
+    loginForm.style.display = 'none';
+    logoutBtn.style.display = 'block';
+  } else {
+    // User is signed out
+    loginForm.style.display = 'block';
+    logoutBtn.style.display = 'none';
+  }
+});
 
-  operationalDays.textContent = operationalDaysCount;
-  outageDays.textContent = outageDaysCount;
-}
+const startInput = document.getElementById("start");
+const endInput = document.getElementById("end");
+const addOutageBtn = document.getElementById("add-outage");
+const outageData = document.getElementById("outage-data");
 
-function saveState() {
-  const outageDates = Array.from(document.querySelectorAll('.day.outage')).map(day => day.dataset.date);
-  localStorage.setItem('outageDates', JSON.stringify(outageDates));
-}
+addOutageBtn.addEventListener('click', async () => {
+  const start = new Date(startInput.value);
+  const end = new Date(endInput.value);
 
-function loadState() {
-  const outageDates = JSON.parse(localStorage.getItem('outageDates')) || [];
-  outageDates.forEach(date => {
-    const dayElement = document.querySelector(`.day[data-date="${date}"]`);
-    if (dayElement) {
-      dayElement.classList.add('outage');
-    }
-  });
-
-  updateDaysCount();
-}
-
-function selectRange(start, end) {
-  const allDays = Array.from(document.querySelectorAll('.day[data-date]'));
-  const startIndex = allDays.indexOf(start);
-  const endIndex = allDays.indexOf(end);
-
-  if (startIndex < 0 || endIndex < 0) {
+  if (start >= end) {
+    alert('Start time must be before end time.');
     return;
   }
 
-  const rangeStart = Math.min(startIndex, endIndex);
-  const rangeEnd = Math.max(startIndex, endIndex);
-  const firstDayIsOutage = start.classList.contains('outage');
+  await addDoc(collection(db, 'outages'), { start, end });
+  updateOutageData();
+  updateCalendar();
+});
+const outagesRef = collection(db, 'outages');
+onSnapshot(outagesRef, () => {
+  updateOutageData();
+  updateCalendar();
+});
 
-  for (let i = rangeStart; i <= rangeEnd; i++) {
-    const dayElement = allDays[i];
-    if (firstDayIsOutage) {
-      dayElement.classList.remove('outage');
-    } else {
-      dayElement.classList.add('outage');
+async function updateOutageData() {
+  const querySnapshot = await getDocs(collection(db, 'outages'));
+  const data = [];
+  querySnapshot.forEach((doc) => {
+    data.push(doc.data());
+  });
+
+  let html = "<table><tr><th>Start</th><th>End</th></tr>";
+  data.forEach((outage) => {
+    const start = new Date(outage.start);
+    const end = new Date(outage.end);
+    html += `<tr><td>${start.toLocaleString()}</td><td>${end.toLocaleString()}</td></tr>`;
+  });
+  html += "</table>";
+  outageData.innerHTML = html;
+}
+
+const calendar = document.getElementById("calendar");
+
+function generateCalendar() {
+  let html = "";
+
+  const firstDay = new Date(2023, 0, 1);
+  const lastDay = new Date(2023, 11, 31);
+
+  for (let date = firstDay; date <= lastDay; ) {
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+
+    if (day === 1) {
+      html += `<h2>${date.toLocaleString("en-US", { month: "long" })} ${year}</h2>`;
+      html += "<table><tr><th>Sun</th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th></tr><tr>";
+    }
+
+    for (let i = 0; i < date.getDay(); i++) {
+      if (day === 1) {
+        html += "<td></td>";
+      }
+    }
+
+    html += `<td id="day-${year}-${month}-${day}" class="day">${day}</td>`;
+
+    if (date.getDay() === 6) {
+      html += "</tr>";
+      if (day + 1 <= new Date(year, month + 1, 0).getDate()) {
+        html += "<tr>";
+      }
+    }
+
+    date.setDate(day + 1);
+
+    if (day === new Date(year, month + 1, 0).getDate()) {
+      html += "</tr></table>";
     }
   }
+
+  calendar.innerHTML = html;
+  attachDayClickListeners();
 }
 
 generateCalendar();
-loadState();
+
+function attachDayClickListeners() {
+  const days = document.getElementsByClassName("day");
+
+  for (const day of days) {
+    day.addEventListener("click", (event) => {
+      if (event.shiftKey && lastClickedDay) {
+        const startId = lastClickedDay.id;
+        const endId = event.target.id;
+        const [startYear, startMonth, startDay] = startId.split("-").slice(1).map(Number);
+        const [endYear, endMonth, endDay] = endId.split("-").slice(1).map(Number);
+
+        let startDate = new Date(startYear, startMonth, startDay);
+        const endDate = new Date(endYear, endMonth, endDay);
+
+        while (startDate <= endDate) {
+          const dayElement = document.getElementById(`day-${startDate.getFullYear()}-${startDate.getMonth()}-${startDate.getDate()}`);
+          dayElement.classList.toggle("outage");
+          startDate.setDate(startDate.getDate() + 1);
+        }
+      } else {
+        event.target.classList.toggle("outage");
+      }
+
+      lastClickedDay = event.target;
+    });
+  }
+}
+
+let lastClickedDay = null
